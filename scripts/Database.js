3e8b610ec50f0a0938059d47e6bcd11a13a9dcdd
JackCallander <z5207123@cse.unsw.edu.au>
2021-06-28 22:58:09 +1000

    Added database

---
+++ scripts/Database.js
@@ -0,0 +1,44 @@
+import Event from "./Event.js";
+
+class Database {
+    constructor() {
+        this._game_keys = [];
+        this._valid_key_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        window.addEventListener("beforeunload", () => this._deconstructor);
+
+        this._fb = firebase.database();
+    }
+
+    NewGame(info) {
+        const game_code = this._GenerateCode(4);
+        info.initial_balls_list.forEach(player => {
+            const player_code = this._GenerateCode(3);
+            this._fb.ref(`games/${game_code}/${player_code}`).set({
+                name: player.name,
+                balls: player.balls,
+            });
+        });
+    }
+
+    EndGame() {
+        this._game_keys.forEach(key => {
+            // Remove all the keys from the database
+        });
+    }
+
+    _GenerateCode(length) {
+        let code = "";
+        for (let i = 0; i < length; i++) {
+            const index = Math.floor(Math.random() * this._valid_key_chars.length);
+            code += this._valid_key_chars.charAt(index);
+        }
+        return code;
+    }
+
+    _deconstructor() {
+        this.EndGame();
+    }
+}
+
+export default Database;
--- scripts/_FrontEnd.js
+++ scripts/_FrontEnd.js
@@ -1,13 +1,14 @@
 // Remove loading screen
-window.onload = function() {
+window.addEventListener("load", () => {
     document.getElementById("modal_loading").style.display = "none";
-}
+});
 
 // Prevent accidental refresh
-window.onbeforeunload = function() {
+window.addEventListener("beforeunload", () => {
     return "Are you sure you want to leave/reload?";
-}
+});
 
+// Close modals that can be closed
 window.onclick = function(e) {
     let m = document.getElementById("modal_newGame");
     if (e.target == m) { m.style.display = "none"; }
@@ -21,3 +22,18 @@ window.onclick = function(e) {
     m = document.getElementById("modal_stats");
     if (e.target == m) { m.style.display = "none"; }
 }
+
+// Connect to Firebase
+// The following connection to firebase has no form of security on it, which is pretty much always bad practice, 
+//  but for this project I doubt this will be a problem.
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD8H1GgwDphlyDsmt2Np0DxsQahR9LRN4c",
+    authDomain: "kellypool.firebaseapp.com",
+    projectId: "kellypool",
+    storageBucket: "kellypool.appspot.com",
+    messagingSenderId: "102507307334",
+    appId: "1:102507307334:web:a7380521c1a878545d4547"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
